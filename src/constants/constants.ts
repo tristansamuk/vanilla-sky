f530b905edb8ee50ccec9a1934589b4d8539624e
Tristan Samuk <tsamuk@gmail.com>
2024-12-21 17:38:45 -0500

    Add basic UI strings

---
+++ src/constants/constants.ts
@@ -0,0 +1,50 @@
+interface UIStrings {
+  [key: string]: string;
+}
+
+export const buttons: UIStrings = {
+  editProfile: 'Edit Profile',
+  newPost: 'New Post',
+};
+
+export const feeds: UIStrings = {
+  discover: 'Discover',
+  film: 'Film',
+  following: 'Following',
+  moreFeeds: 'More feeds',
+  news: 'News',
+  popularWithFriends: 'Popular With Friends',
+  science: 'Science',
+  whatsHotClassic: "What's Hot Classic",
+};
+
+export const inputs: UIStrings = {
+  search: 'Search',
+};
+
+export const navigationMain: UIStrings = {
+  chat: 'Chat',
+  feeds: 'Feeds',
+  home: 'Home',
+  lists: 'Lists',
+  notifications: 'Notifications',
+  profile: 'Profile',
+  search: 'Search',
+  settings: 'Settings',
+};
+
+export const navigationProfile: UIStrings = {
+  feeds: 'Feeds',
+  likes: 'Likes',
+  lists: 'Lists',
+  media: 'Media',
+  posts: 'Posts',
+  replies: 'Replies',
+  starterPacks: 'Starter Packs',
+};
+
+export const profile: UIStrings = {
+  followers: 'followers',
+  following: 'following',
+  posts: 'posts',
+};
