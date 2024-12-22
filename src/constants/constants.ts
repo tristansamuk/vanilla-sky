001c48260484b07cb722015f36326a9cc6c7f178
Tristan Samuk <tsamuk@gmail.com>
2024-12-22 09:48:30 -0500

    Define css variables

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,13 +1,9 @@
-interface UIStrings {
-  [key: string]: string;
-}
-
-export const buttons: UIStrings = {
+export const buttons = {
   editProfile: 'Edit Profile',
   newPost: 'New Post',
 };
 
-export const feeds: UIStrings = {
+export const feeds = {
   discover: 'Discover',
   film: 'Film',
   following: 'Following',
@@ -18,11 +14,11 @@ export const feeds: UIStrings = {
   whatsHotClassic: "What's Hot Classic",
 };
 
-export const inputs: UIStrings = {
+export const inputs = {
   search: 'Search',
 };
 
-export const navigationMain: UIStrings = {
+export const navigationMain = {
   chat: 'Chat',
   feeds: 'Feeds',
   home: 'Home',
@@ -33,7 +29,7 @@ export const navigationMain: UIStrings = {
   settings: 'Settings',
 };
 
-export const navigationProfile: UIStrings = {
+export const navigationProfile = {
   feeds: 'Feeds',
   likes: 'Likes',
   lists: 'Lists',
@@ -43,7 +39,7 @@ export const navigationProfile: UIStrings = {
   starterPacks: 'Starter Packs',
 };
 
-export const profile: UIStrings = {
+export const profile = {
   followers: 'followers',
   following: 'following',
   posts: 'posts',
