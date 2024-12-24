a5aabb7b7e13adf226085d1f43b6eb60fb05f9f6
Tristan Samuk <tsamuk@gmail.com>
2024-12-24 09:09:34 -0500

    Rename naviation files

---
+++ src/components/navigation/navigation.ts
@@ -0,0 +1,82 @@
+import './navigation.css';
+import { navigationMain } from '../../constants/ui-strings';
+import { SVGComponent } from '../svg-component/svg-component';
+import {
+  chat,
+  feeds,
+  home,
+  lists,
+  notifications,
+  profile,
+  search,
+  settings,
+} from '../../constants/svgs';
+
+export class Navigation {
+  private templateEl: HTMLTemplateElement;
+  private hostEl: HTMLElement;
+  private navEl: HTMLElement;
+  private ulEl: HTMLUListElement;
+  private listItemEl: HTMLLIElement;
+  private paragraphEl: HTMLParagraphElement;
+
+  constructor(templateElId = 'nav-template', hostElId = 'app') {
+    this.templateEl = document.getElementById(
+      templateElId
+    ) as HTMLTemplateElement;
+    this.hostEl = document.getElementById(hostElId) as HTMLDivElement;
+    const templateContent = document.importNode(this.templateEl.content, true);
+
+    this.navEl = templateContent.querySelector('.nav') as HTMLElement;
+
+    this.ulEl = templateContent.querySelector('.nav__list') as HTMLUListElement;
+
+    this.listItemEl = templateContent.querySelector(
+      '.nav__item'
+    ) as HTMLLIElement;
+
+    this.paragraphEl = templateContent.querySelector(
+      '.nav__text'
+    ) as HTMLParagraphElement;
+
+    this.render();
+    this.attachIcons();
+  }
+
+  private attachIcons() {
+    // TODO: refactor to make dynamic and put inside render() method
+    new SVGComponent(home, `.nav__icon--home`);
+    new SVGComponent(search, `.nav__icon--search`);
+    new SVGComponent(notifications, `.nav__icon--notifications`);
+    new SVGComponent(chat, `.nav__icon--chat`);
+    new SVGComponent(feeds, `.nav__icon--feeds`);
+    new SVGComponent(lists, `.nav__icon--lists`);
+    new SVGComponent(profile, `.nav__icon--profile`);
+    new SVGComponent(settings, `.nav__icon--settings`);
+  }
+
+  private render() {
+    this.ulEl.innerHTML = '';
+    this.listItemEl.innerHTML = '';
+
+    Object.entries(navigationMain).forEach(([key, value]) => {
+      // Add a class that SVGComponent can use to attach the <svg> icon to the DOM.
+      const iconClass = `nav__icon--${key}`;
+      this.listItemEl.classList.add(iconClass);
+
+      // Copy the <li> and <p>, and set text content
+      const navListItem = this.listItemEl.cloneNode(true);
+      const navItemParagraph = this.paragraphEl.cloneNode(true);
+      navItemParagraph.textContent = value;
+
+      // Attach the <p> to the <li>, and the <li> to the <ul>
+      navListItem.appendChild(navItemParagraph);
+      this.ulEl.appendChild(navListItem);
+
+      // Remove the class from the original element
+      this.listItemEl.classList.remove(iconClass);
+    });
+
+    this.hostEl.appendChild(this.navEl);
+  }
+}
