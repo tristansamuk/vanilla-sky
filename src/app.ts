03e18148496d5f4289dd23cda61180ac2c96a4a7
Tristan Samuk <tsamuk@gmail.com>
2024-12-24 09:09:08 -0500

    Add navigation SVG paths, move SVG creation inside Navigation component

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,5 @@
-import { NavigationDesktop } from './components/navigation-desktop/navigation-desktop';
-import { SVGComponent } from './components/svg-component/svg-component';
-import { home, search } from './constants/svgs';
+import { Navigation } from './components/navigation/navigation';
 
-// Render Desktop Navigation
+// Render navigation and attach icons
 
-new NavigationDesktop();
-new SVGComponent(home, `.nav__icon--home`);
-new SVGComponent(search, `.nav__icon--search`);
+new Navigation();
