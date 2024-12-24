540ba1b13283dd0d446580dbfdd954f06b4c2b51
Tristan Samuk <tsamuk@gmail.com>
2024-12-24 17:59:15 -0500

    A bunch css changes to navigation element and create page layout component

---
+++ src/components/page-layout/page-layout.ts
@@ -0,0 +1,25 @@
+import './page-layout.css';
+
+export class PageLayout {
+  private hostEl: HTMLElement;
+  private templateEl: HTMLTemplateElement;
+  private pageLayout: HTMLDivElement;
+
+  constructor(hostElId: string) {
+    this.hostEl = document.getElementById(hostElId) as HTMLElement;
+    this.templateEl = document.getElementById(
+      'page-layout-template'
+    ) as HTMLTemplateElement;
+    const templateContent = document.importNode(this.templateEl.content, true);
+
+    this.pageLayout = templateContent.querySelector(
+      '.page-layout'
+    ) as HTMLDivElement;
+
+    this.render();
+  }
+
+  private render() {
+    this.hostEl.appendChild(this.pageLayout);
+  }
+}
